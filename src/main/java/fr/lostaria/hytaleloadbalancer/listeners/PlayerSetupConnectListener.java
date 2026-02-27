19476d753687e394a51ac9937c2ba8be04b76087
Erpriex <erpriex@gmail.com>
2026-02-27 19:22:24 +0100

    💄 Format refuse connection message & deperecated IP

--- src/main/java/fr/lostaria/hytaleloadbalancer/listeners/PlayerSetupConnectListener.java
+++ src/main/java/fr/lostaria/hytaleloadbalancer/listeners/PlayerSetupConnectListener.java
@@ -6,7 +6,15 @@ public class PlayerSetupConnectListener {
 
     public static void onPlayerSetupConnect(PlayerSetupConnectEvent event) {
         event.setCancelled(true);
-        event.setReason("Infrastructure en cours de developpement");
+        if(event.getPacketHandler().getSniHostname().equalsIgnoreCase("hytale.lostaria.fr")) {
+            event.setReason(formatReason("Cette adresse IP ne sera bientôt plus prise en charge\nMerci d'utiliser dorénavant : play.lostaria.fr"));
+        } else {
+            event.setReason(formatReason("Infrastructure en cours de developpement\nOn arrive vite ! ;)"));
+        }
+    }
+
+    private static String formatReason(String reason) {
+        return "\n——————————— Lostaria ———————————\n\n" + reason + "\n\n———————————————————————————\n";
     }
 
 }
